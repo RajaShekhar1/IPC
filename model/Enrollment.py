32afafee12c0168917a728c7b13108bc363d42d3
Bill Davis <bdavis@thumbprintcpm.com>
2014-06-26 19:45:26 -0400

    new form control for Enrollment Setup
    new means to render the Setup Enrollment via WTF Forms with disabled
    items.  Still need to add javascript to dynamically swap list of state
    options dependent on product selected; or perhaps we’ll just
    enable/disable on same list (TBD)

--- model/Enrollment.py
+++ model/Enrollment.py
@@ -9,6 +9,12 @@ from dateutil.relativedelta import relativedelta
 from mailer import Mailer, Message
 from flask.ext.stormpath import user
 
+from flask.ext.wtf import Form
+from wtforms.fields import StringField, SelectField
+from wtforms.validators import InputRequired, Email
+from wtforms.widgets import html_params, HTMLString
+from jinja2 import escape
+
 
 class Case(object):
     def __init__(self, id, company_name, situs_state, product):
@@ -170,3 +176,184 @@ class NotifyAdminEmail(object):
         connection.sendmail(self.smtp_user, to_user, msg.as_string())
         connection.close()
         
+
+#
+# SelectWithDisable and SelectFieldWithDisable from http://stackoverflow.com/questions/8463421/how-to-render-my-select-field-with-wtforms
+#  although I added a bogus X parm in for loop in middle of __call__ method below to accommodate the coerce value in tuple
+#
+class SelectWithDisable(object):
+    """
+    Renders a select field.
+
+    If `multiple` is True, then the `size` property should be specified on
+    rendering to make the field useful.
+
+    The field must provide an `iter_choices()` method which the widget will
+    call on rendering; this method must yield tuples of 
+    `(value, label, selected, disabled)`.
+    """
+    def __init__(self, multiple=False):
+        self.multiple = multiple
+
+    def __call__(self, field, **kwargs):
+        kwargs.setdefault('id', field.id)
+        if self.multiple:
+            kwargs['multiple'] = 'multiple'
+        html = [u'<select %s>' % html_params(name=field.name, **kwargs)]
+        for val, label, selected, disabled, x in field.iter_choices():
+            html.append(self.render_option(val, label, selected, disabled))
+        html.append(u'</select>')
+        return HTMLString(u''.join(html))
+
+    @classmethod
+    def render_option(cls, value, label, selected, disabled):
+        options = {'value': value}
+        if selected:
+            options['selected'] = u'selected'
+        if disabled:
+            options['disabled'] = u'disabled'
+        return HTMLString(u'<option %s>%s</option>' % (html_params(**options), escape(unicode(label))))
+
+
+class SelectFieldWithDisable(SelectField):
+    widget = SelectWithDisable()
+
+    def iter_choices(self):
+        for value, label, selected, disabled in self.choices:
+            yield (value, label, selected, disabled, self.coerce(value) == self.data)
+
+
+#
+# each set of option values for respective Products is a tuple of 
+# value, name, selected (True/False), disabled (True/False)) 
+#
+#[('1', 'VEHICLES', False, True), ('2', 'Cars', False, False), ('3', 'Motorcycles', False, False)]
+#
+FPPTI_states = [
+    ("", ' ', False, False),
+    ('AL', 'Alabama', False, False),
+    ('AK', 'Alaska', False, False),
+    ('AZ', 'Arizona', False, False),
+    ('AR', 'Arkansas', False, False),
+    ('CA', 'California', False, False),
+    ('CO', 'Colorado', False, True),
+    ('CT', 'Connecticut', False, True),
+    ('DE', 'Delaware', False, False),
+    ('FL', 'Florida', False, True),
+    ('GA', 'Georgia', False, False),
+    ('HI', 'Hawaii', False, False),
+    ('ID', 'Idaho', False, False),
+    ('IL', 'Illinois', False, True),
+    ('IN', 'Indiana', False, True),
+    ('IA', 'Iowa', False, False),
+    ('KS', 'Kansas', False, False),
+    ('KY', 'Kentucky', False, False),
+    ('LA', 'Louisiana', False, False),
+    ('ME', 'Maine', False, True),
+    ('MD', 'Maryland', False, True),
+    ('MA', 'Massachusetts', False, True),
+    ('MI', 'Michigan', False, False),
+    ('MN', 'Minnesota', False, True),
+    ('MS', 'Mississippi', False, False),
+    ('MO', 'Missouri', False, True),
+    ('MT', 'Montana', False, False),
+    ('NE', 'Nebraska', False, False),
+    ('NV', 'Nevada', False, False),
+    ('NH', 'New Hampshire', False, True),
+    ('NJ', 'New Jersey', False, True),
+    ('NM', 'New Mexico', False, False),
+    ('NY', 'New York', False, True),
+    ('NC', 'North Carolina', False, True),
+    ('ND', 'North Dakota', False, True),
+    ('OH', 'Ohio', False, True),
+    ('OK', 'Oklahoma', False, False),
+    ('OR', 'Oregon', False, False),
+    ('PA', 'Pennsylvania', False, True),
+    ('RI', 'Rhode Island', False, False),
+    ('SC', 'South Carolina', False, False),
+    ('SD', 'South Dakota', False, False),
+    ('TN', 'Tennessee', False, False),
+    ('TX', 'Texas', False, False),
+    ('UT', 'Utah', False, False),
+    ('VT', 'Vermont', False, False),
+    ('VA', 'Virginia', False, False),
+    ('WA', 'Washington', False, False),
+    ('WV', 'West Virginia', False, False),
+    ('WI', 'Wisconsin', False, False),
+    ('WY', 'Wyoming', False, False)
+]
+
+FPPCI_states = [
+    ("", ' ', False, False),
+    ('AL', 'Alabama', False, False),
+    ('AK', 'Alaska', False, False),
+    ('AZ', 'Arizona', False, False),
+    ('AR', 'Arkansas', False, False),
+    ('CA', 'California', False, False),
+    ('CO', 'Colorado', False, False),
+    ('CT', 'Connecticut', False, False),
+    ('DE', 'Delaware', False, False),
+    ('FL', 'Florida', False, False),
+    ('GA', 'Georgia', False, False),
+    ('HI', 'Hawaii', False, False),
+    ('ID', 'Idaho', False, False),
+    ('IL', 'Illinois', False, False),
+    ('IN', 'Indiana', False, False),
+    ('IA', 'Iowa', False, False),
+    ('KS', 'Kansas', False, False),
+    ('KY', 'Kentucky', False, False),
+    ('LA', 'Louisiana', False, False),
+    ('ME', 'Maine', False, False),
+    ('MD', 'Maryland', False, False),
+    ('MA', 'Massachusetts', False, False),
+    ('MI', 'Michigan', False, False),
+    ('MN', 'Minnesota', False, False),
+    ('MS', 'Mississippi', False, False),
+    ('MO', 'Missouri', False, False),
+    ('MT', 'Montana', False, False),
+    ('NE', 'Nebraska', False, False),
+    ('NV', 'Nevada', False, False),
+    ('NH', 'New Hampshire', False, False),
+    ('NJ', 'New Jersey', False, False),
+    ('NM', 'New Mexico', False, False),
+    ('NY', 'New York', False, False),
+    ('NC', 'North Carolina', False, False),
+    ('ND', 'North Dakota', False, False),
+    ('OH', 'Ohio', False, False),
+    ('OK', 'Oklahoma', False, False),
+    ('OR', 'Oregon', False, False),
+    ('PA', 'Pennsylvania', False, False),
+    ('RI', 'Rhode Island', False, False),
+    ('SC', 'South Carolina', False, False),
+    ('SD', 'South Dakota', False, False),
+    ('TN', 'Tennessee', False, False),
+    ('TX', 'Texas', False, False),
+    ('UT', 'Utah', False, False),
+    ('VT', 'Vermont', False, False),
+    ('VA', 'Virginia', False, False),
+    ('WA', 'Washington', False, False),
+    ('WV', 'West Virginia', False, False),
+    ('WI', 'Wisconsin', False, False),
+    ('WY', 'Wyoming', False, False)
+]
+    
+
+    
+
+
+product_choices = [(""," ", False, True),
+                   ("FPPTI","Family Protection Plan - Term to 100", False, False),
+                   ("FPPCI","Family Protection Plan - with Critical Illness (coming soon)", False, True),
+                   ("CIEMP","Group Critical Illness (coming soon)", False, True),
+                   ("VGL", "Voluntary Group Life (coming soon)", False, True)
+               ]
+
+class EnrollmentSetupForm(Form):
+    productID = SelectFieldWithDisable(choices = product_choices)
+    enrollmentState = SelectFieldWithDisable(choices = FPPTI_states)
+    fppciState = SelectFieldWithDisable(choices = FPPCI_states)
+    companyName = StringField('Company Name', validators=[InputRequired()])
+    eeFName = StringField('Employee First Name', validators=[InputRequired()])
+    eeLName = StringField('Employee Last Name', validators=[InputRequired()])
+    email = StringField('Employee email', validators=[Email()])
+
