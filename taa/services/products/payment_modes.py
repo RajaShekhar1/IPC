bfb695235da73df00ff4aca5afd62ee9fc155542
Jeff Kayser <jeff.kayser@gmail.com>
2015-04-21 03:24:39 -0400

    Correct spelling of "biweekly" and "semimonthly".

--- taa/services/products/payment_modes.py
+++ taa/services/products/payment_modes.py
@@ -9,8 +9,8 @@ MODE_CHANGEABLE = -1
 # When `adhoc` is false, the option is not permitted during ad-hoc enrollment
 payment_modes = map(lambda x: dict(mode=x[0], name=x[1], immutable=x[2]), [
     (MODE_WEEKLY, 'Weekly', True),
-    (MODE_BIWEEKLY, 'Bi-Weekly', True),
-    (MODE_SEMIMONTHLY, 'Semi-Monthly', True),
+    (MODE_BIWEEKLY, 'Biweekly', True),
+    (MODE_SEMIMONTHLY, 'Semimonthly', True),
     (MODE_MONTHLY, 'Monthly', True),
     (MODE_CHANGEABLE, 'Leave For Applicant To Select', False),
     ])
