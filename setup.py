8b07191d6f12562cdf2a1c03c6458f2f251e1f5d
Jeff Kayser <jeff.kayser@gmail.com>
2015-04-28 01:21:03 -0400

    Add initial tests for rate table.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='taa',
+      version='1.0',
+      py_modules=['taa'],
+      description='5Star Take-an-App',
+      author='DelMar Software Development',
+      url='https://5starenroll.com',
+      )
