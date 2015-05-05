e7a3c0d07843bb13a11f607461797749ad718dd2
Zach Mason <zmason@delmarsd.com>
2015-05-05 11:42:18 -0400

    Altered the previous fix to hide dynamic forms before adding them to the DOM

--- taa/frontend/static/js/5Star/util.js
+++ taa/frontend/static/js/5Star/util.js
@@ -132,7 +132,7 @@ function submit_to_url(url, data) {
     form.action = url;
 
     // Need to add to the DOM for firefox and ie
-    $(form).appendTo("body").submit();
+    $(form).wrap("<div style='display: none;'></div>").appendTo("body").submit();
 }
 
 
