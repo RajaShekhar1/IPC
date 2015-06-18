5cc98921d6d3e20bcb359e15d9839b27916bb8ce
Jeff Kayser <jeff.kayser@gmail.com>
2015-06-18 12:24:05 -0400

    Add self-enrollment for census-based (targeted).

--- taa/frontend/views/enrollment.py
+++ taa/frontend/views/enrollment.py
@@ -7,7 +7,8 @@ import json
 
 import requests
 
-from flask import request, render_template, jsonify, session, send_from_directory, url_for
+from flask import (abort, jsonify, render_template, request,
+                   send_from_directory, session, url_for)
 from flask.ext.stormpath import login_required
 from flask_stormpath import current_user
 
@@ -62,15 +63,34 @@ def enroll_start():
 @app.route('/in-person-enrollment', methods=['POST'])
 @login_required
 def in_person_enrollment():
-    if request.form.get('record_id'):
+    record_id = request.form.get('record_id')
+    is_self_enroll = session.get('is_self_enrollment', False)
+    if record_id is not None:
+        # In-person enrollment
+        return _in_person_enrollment(record_id=int(record_id),
+                                     is_self_enroll=is_self_enroll)
+    else:
+        # Ad-hoc enrollment
+        return _in_person_enrollment(data=request.form,
+                                     is_self_enroll=is_self_enroll)
+
+
+def _in_person_enrollment(record_id=None, data=None, is_self_enroll=False):
+    is_in_person = True
+    if record_id is not None:
         # Enrolling from a case census record
-        record_id = int(request.form['record_id'])
+        record_id = int(record_id)
         record = case_service.get_census_record(None, record_id)
         # Set a flag that we are currently enrolling from this case
         session['active_case_id'] = record.case_id
         session['enrolling_census_record_id'] = record.id
-        state = record.case.situs_state
-        enroll_city = record.case.situs_city
+        if is_self_enroll:
+            state = record.employee_state or record.case.situs_state
+            enroll_city = record.employee_city or record.case.situs_city
+            is_in_person = False
+        else:
+            state = record.case.situs_state
+            enroll_city = record.case.situs_city
         company_name = record.case.company_name
         group_number = record.case.group_number
         products = record.case.products
@@ -86,7 +106,6 @@ def in_person_enrollment():
             payment_mode_choices = get_payment_modes(single=payment_mode)
     else:
         # Ad-hoc enrollment
-        data = request.form
         state = data['enrollmentState']
         enroll_city = data['enrollmentCity']
         company_name = data['companyName']
@@ -125,7 +144,7 @@ def in_person_enrollment():
         'employee_data': employee_data,
         'spouse_data': spouse_data,
         'children_data': children_data,
-        'is_in_person': True,
+        'is_in_person': is_in_person,
         'health_questions': soh_questions,
         'payment_mode_choices': payment_mode_choices,
         'payment_mode': payment_mode,
@@ -146,21 +165,26 @@ def self_enrollment(company_name, uuid):
                                                                                      current_user.is_anonymous())
     vars = {'is_valid': False}
     if setup is not None:
+        session['is_self_enroll'] = True
         vars.update({
             'is_valid': True,
             'page_title': setup.page_title,
             'page_text': setup.page_text,
             'page_disclaimer': setup.page_disclaimer,
             'census_record': census_record,
+            'self_enrollment_setup': setup,
         })
     return render_template('enrollment/landing_page.html', **vars)
 
 
-@app.route('/self-enroll', methods=['PUT'])
+@app.route('/self-enrollment', methods=['POST'])
 def self_enrollment2():
-    if request.form.get('record_id'):
-        pass
-    pass
+    id = request.form.get('record_id')
+    if session.get('is_self_enroll') is not None:
+        return _in_person_enrollment(record_id=int(id) if id else None,
+                                     is_self_enroll=True)
+    else:
+        abort(404)
 
 
 @app.route('/submit-wizard-data', methods=['POST'])
