3547b0c3e5094c527fe450374fce5f5b1de467e6
Zach Mason <zmason@delmarsd.com>
2015-06-26 00:56:09 -0400

    Fixed some errors for links

--- taa/services/enrollments/__init__.py
+++ taa/services/enrollments/__init__.py
@@ -856,12 +856,19 @@ class SelfEnrollmentLinkService(DBService):
         if link is None:
             # Link no longer exists
             return None, None
+
+        # make sure the link is setup properly
+        if not link.self_enrollment_setup:
+            return None, None
+
         if not link.self_enrollment_setup.case.active:
             # Case is not active
             return None, None
+
         if not link.self_enrollment_setup.case.is_self_enrollment:
             # Self-enrollment is not active
             return None, None
+        
         if increment_clicks:
             link.clicks += 1
             db.session.commit()
