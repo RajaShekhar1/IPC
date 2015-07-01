3f37130a4368228f1e9889490f6d62a8bc51b568
Zach Mason <zmason@delmarsd.com>
2015-07-01 14:45:50 -0400

    Fixed my last merge.

--- taa/frontend/static/js/5Star/case_management.js
+++ taa/frontend/static/js/5Star/case_management.js
@@ -2,7 +2,7 @@ var case_management = (function() {
 
     var loading_html = "<span class='icon-spinner icon-spin grey bigger-200'></span> <span class='bigger-175'> Loading data...</span>";
 
-    function refresh_census_table(case_id, url, table_selector, loading_selector, table_options, init_callback, no_data_cb) {
+    function refresh_census_table(case_id, url, table_selector, loading_selector, init_callback, no_data_cb) {
 
         // show loading message under the table
         var loading = $(loading_selector);
