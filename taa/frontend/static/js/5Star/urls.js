0e58c1bfe8b8d08fe02635935c524f3948501cdc
Zach Mason <zmason@delmarsd.com>
2015-07-03 15:49:51 -0400

    Cleanup code formatting, extract some JS files from case.html, added URL lookup mechanism for js

---
+++ taa/frontend/static/js/5Star/urls.js
@@ -0,0 +1,28 @@
+var urls = (function() {
+  return {
+    get_manage_case_url: function(case_id) {
+      return '/enrollment-case/' + case_id;
+    },
+    get_cases_api_url: function() {
+      return "/cases";
+    },
+    get_case_api_url: function(case_id) {
+      return '/cases/' + case_id;
+    },
+    get_case_api_enrollment_periods_url: function(case_id) {
+      return '/cases/' + case_id + '/enrollment_periods';
+    },
+    get_case_api_self_enrollment_url: function(case_id) {
+      return '/cases/' + case_id + '/self_enrollment_setup';
+    },
+    get_case_api_census_records_url: function(case_id) {
+      return '/cases/' + case_id + '/census_records';
+    },
+    get_case_api_census_record_url: function(case_id, census_record_id) {
+      return '/cases/' + case_id + '/census_records/' + census_record_id;
+    },
+    get_product_api_url: function(product_id) {
+      return '/products/' + case_id;
+    }
+  }
+})();
