e7255d5f3bdcc1322f63878ba712564c1889efa1
Zach Mason <zmason@delmarsd.com>
2015-07-22 11:16:16 -0400

    Modified cert generating script with days and remove unused parts of key, updated gitignore to ignore cert

--- FTPDropBox/generatePEM.sh
+++ FTPDropBox/generatePEM.sh
@@ -1,4 +1,5 @@
 openssl genrsa -out taa.key 1024
 openssl req -new -key taa.key -out taa.csr
-openssl x509 -req -days 365 -in taa.csr -signkey taa.key -out taa.crt
+openssl x509 -req -days 1095 -in taa.csr -signkey taa.key -out taa.crt
 cat taa.key taa.crt > taa.pem
+rm -f taa.key taa.crt taa.csr
