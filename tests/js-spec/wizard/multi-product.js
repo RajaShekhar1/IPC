6357a6cc5e1f33163b00554a899acacc2cd35031
Zach Mason <zmason@delmarsd.com>
2015-07-28 10:36:39 -0400

    Added stub test for multi-product

--- tests/js-spec/wizard/multi-product.js
+++ tests/js-spec/wizard/multi-product.js
@@ -1,3 +1,18 @@
-/**
- * Created by zach on 7/27/15.
- */
+describe("Enroll multiple products ", function(){
+    var vm;
+    beforeEach(function() {
+        vm = new wizard_viewmodel.WizardUI({
+          products: [{
+            is_fpp_product: true,
+          }],
+          children_data: [],
+        });
+    });
+
+    it("should format 0 seconds as '00:00'", function() {
+
+        var result = vm.NAIC_AND_MI[0];
+
+        expect(result).toEqual("AK");
+    });
+});
