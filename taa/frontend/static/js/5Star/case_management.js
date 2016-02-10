6e47ee9795ad073edd6bf9ed62a8996ecba75f8a
Zach Mason <zmason@delmarsd.com>
2016-02-10 10:05:11 -0500

    Built assets

--- taa/frontend/static/js/5Star/case_management.js
+++ taa/frontend/static/js/5Star/case_management.js
@@ -31,11 +31,7 @@ var case_management = (function() {
               {"aTargets":[4], "mData": function(source) {
                 return normalize_date(source.employee_birthdate);
               }},
-              {"aTargets":[5], "mData": function(source) {
-
-                  return source.email;
-                  //return source.
-              }}
+              {"aTargets":[5], "mData": "agent", className: "min-breakIII"}
               //{"aTargets":[5], "mData":"employee_email", className: "min-breakIII"}
             ],
             "aaSorting": [[ 3, "asc" ]]
