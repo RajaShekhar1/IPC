3c53a43f31dd2d754f3a700a79f91b9a63e17974
Zach Mason <zmason@delmarsd.com>
2016-02-10 09:59:19 -0500

    build assets for staging

--- taa/frontend/static/js/5Star/case_management.js
+++ taa/frontend/static/js/5Star/case_management.js
@@ -31,7 +31,12 @@ var case_management = (function() {
               {"aTargets":[4], "mData": function(source) {
                 return normalize_date(source.employee_birthdate);
               }},
-              {"aTargets":[5], "mData":"employee_email", className: "min-breakIII"}
+              {"aTargets":[5], "mData": function(source) {
+
+                  return source.email;
+                  //return source.
+              }}
+              //{"aTargets":[5], "mData":"employee_email", className: "min-breakIII"}
             ],
             "aaSorting": [[ 3, "asc" ]]
           };
