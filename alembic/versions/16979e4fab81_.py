b745c37b575dcfc9c7a44183d9c57a25a7767427
Zach Mason <zmason@delmarsd.com>
2016-02-18 14:27:33 -0500

    Merged alembic heads from Conlin's work on product names

---
+++ alembic/versions/16979e4fab81_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 16979e4fab81
+Revises: ('3ba1da7b91a9', '4f7abbcbac0f')
+Create Date: 2016-02-18 14:26:54.583237
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '16979e4fab81'
+down_revision = ('3ba1da7b91a9', '4f7abbcbac0f')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
