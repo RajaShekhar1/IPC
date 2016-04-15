3d81d78e365b9e59c47ba90b37078ef0471e07ad
Michael Nowak <mnowak@delmarsd.com>
2016-04-15 14:36:48 -0400

    If omit_actively_at_work is true for an fpp case then the docusign checkboxes will both be empty. Closes #615

--- taa/services/docusign/docusign_envelope.py
+++ taa/services/docusign/docusign_envelope.py
@@ -129,8 +129,6 @@ class EnrollmentDataWrap(object):
             return agent_service.get_logged_in_agent()
         elif self.data.get('agent_id'):
             return agent_service.get(self.data['agent_id'])
-        elif not flask.has_request_context():
-            return None
         else:
             # If the logged-in user is not an agent, default to case owner.
             return self.case.owner_agent
@@ -376,6 +374,11 @@ class EnrollmentDataWrap(object):
 
     def should_use_call_center_workflow(self):
         return self.case.should_use_call_center_workflow
+
+    def get_actively_at_work(self):
+        if self.get_product().is_fpp() and self.case.omit_actively_at_work:
+            return ''
+        return 'yes' if self.data['is_employee_actively_at_work'] else 'no'
 
 # For employee signing sessions
 def build_callback_url(wizard_data, session_type):
