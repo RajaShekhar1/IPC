e1625ae42eaabc391d4882dd7442b99e23b78155
Zach Mason <zmason@delmarsd.com>
2016-04-15 11:48:12 -0400

    fixed error in patch

--- taa/old_model/States.py
+++ taa/old_model/States.py
@@ -4,6 +4,6 @@ from taa.services.products.states import get_all_states
 def get_states():
     "Backwards-compatible formatted states for wizard."
     return [
-        {"code": state[0], "name": state[1]}
+        {"code": state['statecode'], "name": state['name']}
         for state in get_all_states()
     ]
