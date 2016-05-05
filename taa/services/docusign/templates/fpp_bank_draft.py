56377d9b2a5e2a18700e55de65032fdb87788bdb
Michael Nowak <mnowak@delmarsd.com>
2016-05-05 16:25:57 -0400

    Added full bank draft information if it exists on the enrollment_application. The new data is used instead of the employee's address, city, etc... if available. If an older application is submitted it will use the older method of setting the information for the bank draft form.

--- taa/services/docusign/templates/fpp_bank_draft.py
+++ taa/services/docusign/templates/fpp_bank_draft.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from dateutil.relativedelta import relativedelta
 
-from taa.services.docusign.service import DocuSignServerTemplate, DocuSignTextTab
+from taa.services.docusign.service import DocuSignServerTemplate, DocuSignTextTab, DocuSignRadioTab
 from taa.services.docusign.DocuSign_config import get_bank_draft_template_id
 
 
@@ -22,22 +22,85 @@ class FPPBankDraftFormTemplate(DocuSignServerTemplate):
         if not recipient.is_employee() and not self.data.should_use_call_center_workflow():
             return tabs
 
-        employee_data = self.data["employee"]
-        address = employee_data.get('address1', '')
-        if employee_data.get('address2'):
-            address += " " + employee_data.get('address2', '')
+        # Going forward enrollments will have bank draft data and should grab all account information from that
+        if self.has_bank_draft_info():
+            # New method that grabs data from the bank draft info entered in on the last step of the wizard
+            tabs += [
+                DocuSignTextTab('eeName', self.get_bank_account_holder()),
+                DocuSignTextTab('eeAddress', self.get_account_address()),
+                DocuSignTextTab('eeCity', self.get_account_city()),
+                DocuSignTextTab('eeState', self.get_account_state()),
+                DocuSignTextTab('eeZip', self.get_account_zip()),
+                DocuSignTextTab('MonthlyPremium', self.get_monthly_premium()),
+                DocuSignTextTab('DraftDay', self.get_draft_day()),
+                DocuSignTextTab('RoutingNumber', self.get_bank_routing_number()),
+                DocuSignTextTab('BankName', self.get_bank_name()),
+                DocuSignTextTab('Account Number', self.get_bank_account_number()),
+                DocuSignTextTab('CityStateZip', self.get_city_state_zip()),
+                DocuSignRadioTab('AccountType', self.get_bank_account_type())
+            ]
+        else:
+            # Old version to work with previously existing enrollments
+            employee_data = self.data["employee"]
+            address = employee_data.get('address1', '')
+            if employee_data.get('address2'):
+                address += " " + employee_data.get('address2', '')
 
-        tabs += [
-            DocuSignTextTab('eeName', self.data.get_employee_name()),
-            DocuSignTextTab('eeAddress', address),
-            DocuSignTextTab('eeCity', employee_data.get('city', '')),
-            DocuSignTextTab('eeState', employee_data.get('state', '')),
-            DocuSignTextTab('eeZip', employee_data.get('zip', '')),
-            DocuSignTextTab('MonthlyPremium', self.get_monthly_premium()),
-            DocuSignTextTab('DraftDay', self.get_draft_day()),
-        ]
+            tabs += [
+                DocuSignTextTab('eeName', self.data.get_employee_name()),
+                DocuSignTextTab('eeAddress', address),
+                DocuSignTextTab('eeCity', employee_data.get('city', '')),
+                DocuSignTextTab('eeState', employee_data.get('state', '')),
+                DocuSignTextTab('eeZip', employee_data.get('zip', '')),
+                DocuSignTextTab('MonthlyPremium', self.get_monthly_premium()),
+                DocuSignTextTab('DraftDay', self.get_draft_day()),
+            ]
+
 
         return tabs
+
+    def has_bank_draft_info(self):
+        return self.data.get('bank_info', None) is not None
+
+    def get_bank_account_type(self):
+        return self.data['bank_info'].get('account_type', '')
+
+    def get_bank_account_holder(self):
+        return self.data['bank_info'].get('account_holder_name', '')
+
+    def get_bank_account_number(self):
+        return self.data['bank_info'].get('account_number', '')
+
+    def get_bank_routing_number(self):
+        return self.data['bank_info'].get('routing_number', '')
+
+    def get_bank_name(self):
+        return self.data['bank_info'].get('bank_name', '')
+
+    def get_account_address_one(self):
+        return self.data['bank_info'].get('address_one', '')
+
+    def get_account_address_two(self):
+        return self.data['bank_info'].get('address_two', '')
+
+    def get_account_address(self):
+        address_one = self.get_account_address_one()
+        address_two = self.get_account_address_two()
+        if len(address_two) > 0:
+            return '%s %s' % (address_one, address_two)
+        return address_one
+
+    def get_account_city(self):
+        return self.data['bank_info'].get('city', '')
+
+    def get_account_state(self):
+        return self.data['bank_info'].get('state', '')
+
+    def get_account_zip(self):
+        return self.data['bank_info'].get('zip', '')
+
+    def get_city_state_zip(self):
+        return '%s, %s %s' % (self.get_account_city(), self.get_account_state(), self.get_account_zip())
 
     def get_monthly_premium(self):
         # For now, just add the premiums, since we know they are monthly payment mode.
