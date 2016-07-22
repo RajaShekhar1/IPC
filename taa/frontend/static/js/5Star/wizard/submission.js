155acff2284e72b032a0ccac76890bcf8e949659
coleelam <elamc@purdue.edu>
2016-07-22 14:03:36 -0400

    fix things mentioned in commit comments

--- taa/frontend/static/js/5Star/wizard/submission.js
+++ taa/frontend/static/js/5Star/wizard/submission.js
@@ -173,7 +173,7 @@ function build_wizard_results_for_product_coverage(product_cov) {
     identityToken: root.identityToken(),
     identityType: root.identityType(),
     
-    send_summary_email: root.email_summary_sheet_confirmed(),
+    send_summary_email: root.should_email_summary_sheet(),
     summaryEmail: root.get_summary_email(),
 
     employee: root.employee().serialize_data(),
