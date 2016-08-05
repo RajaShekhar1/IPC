237260c0d2dd165d73d8c568732a972ba92e291b
coleelam <celam@delmarit.com>
2016-08-05 15:08:14 -0400

    Closes #753. finish case conversion script

--- taa/manage/CaseConversion.py
+++ taa/manage/CaseConversion.py
@@ -1,8 +1,7 @@
 from flask_script import Command, Option
 from taa import db
 from taa.models import (Case, CaseEnrollmentPeriod, CaseOpenEnrollmentPeriod, CaseOngoingEnrollmentPeriod,
-                        CaseBothEnrollmentPeriod)
-import json
+                        CaseBothEnrollmentPeriod, CaseAnnualEnrollmentPeriod)
 
 
 class CaseConversionCommand(Command):
@@ -15,12 +14,13 @@ class CaseConversionCommand(Command):
 
 
 def convert_cases():
+    """
+    converts cases to be compatible with effective date capabilities
+    """
+
     s = db.session
     cases = s.query(Case).all()
-
-    def create_ongoing_cep(case_id):
-        new_cep = CaseEnrollmentPeriod(case_id=case_id, period_type="ongoing", start_date=None, end_date=None)
-        return new_cep
+    case_list = []
 
     for case in cases:
         has_open = False
@@ -34,18 +34,16 @@ def convert_cases():
 
         for cep in case_enrollment_periods:
             if cep.period_type == CaseOpenEnrollmentPeriod.PERIOD_TYPE and not cep.start_date and not cep.end_date:
-                has_open = True
                 continue
             elif cep.period_type == CaseOngoingEnrollmentPeriod.PERIOD_TYPE:
-                has_ongoing = True
                 continue
             elif cep.period_type == CaseOpenEnrollmentPeriod.PERIOD_TYPE and cep.start_date and not cep.end_date:
                 has_open = True
                 has_ongoing = True
-                cep.end_date, start_date, end_date = cep.start_date
+                cep.end_date = start_date = end_date = cep.start_date
                 ongoing_cep = create_ongoing_cep(case.id)
                 s.add(ongoing_cep)
-            elif cep.period_type == "annual":
+            elif cep.period_type == CaseAnnualEnrollmentPeriod.PERIOD_TYPE:
                 has_ongoing = True
                 annual_periods.append(cep)
             elif (cep.period_type == "open" or cep.period_type == CaseOpenEnrollmentPeriod.PERIOD_TYPE) and cep.start_date and cep.end_date:
@@ -81,7 +79,17 @@ def convert_cases():
         if not case.effective_date_settings and (has_open or has_ongoing):
             case.effective_date_settings = format_effective_date_settings(has_open, has_ongoing, is_paylogix,
                                                                           start_date, end_date)
-            case.product_settings = format_product_settings(case.product_settings, case_product_ids)
+            # case.product_settings = format_product_settings(case.product_settings, case_product_ids)
+
+        case_list.append(case.id)
+
+    print ("Updating Cases: {}".format(case_list))
+    s.commit()
+
+
+def create_ongoing_cep(case_id):
+    new_cep = CaseEnrollmentPeriod(case_id=case_id, period_type="ongoing", start_date=None, end_date=None)
+    return new_cep
 
 
 def format_effective_date_settings(has_open, has_ongoing, is_paylogix, start_date, end_date):
@@ -93,7 +101,7 @@ def format_effective_date_settings(has_open, has_ongoing, is_paylogix, start_dat
         else:
             effective_date_settings = [{"type": "open", "method": Case.DAY_OF_MONTH_ENROLLMENT_TYPE,
                                         Case.DAY_OF_MONTH_ENROLLMENT_TYPE: 15}]
-        return json.dumps(effective_date_settings)
+        return effective_date_settings
     elif has_ongoing and not has_open:
         if is_paylogix:
             effective_date_settings = [{"type": "ongoing", "method": Case.FIRST_FRIDAY_ENROLLMENT_TYPE,
@@ -101,7 +109,7 @@ def format_effective_date_settings(has_open, has_ongoing, is_paylogix, start_dat
         else:
             effective_date_settings = [{"type": "ongoing", "method": Case.DAY_OF_MONTH_ENROLLMENT_TYPE,
                                         Case.DAY_OF_MONTH_ENROLLMENT_TYPE: 15}]
-        return json.dumps(effective_date_settings)
+        return effective_date_settings
     elif has_open and has_ongoing:
         if is_paylogix:
             effective_date_settings = [
@@ -113,7 +121,7 @@ def format_effective_date_settings(has_open, has_ongoing, is_paylogix, start_dat
                 {"type": "open", "method": Case.DAY_OF_MONTH_ENROLLMENT_TYPE, Case.DAY_OF_MONTH_ENROLLMENT_TYPE: 15,
                  "enrollment_period": {"start_date": start_date.strftime("%x"), "end_date": end_date.strftime("%x")}},
                 {"type": "ongoing", "method": Case.DAY_OF_MONTH_ENROLLMENT_TYPE, Case.DAY_OF_MONTH_ENROLLMENT_TYPE: 15}]
-        return json.dumps(effective_date_settings)
+        return effective_date_settings
     else:
         return None
 
