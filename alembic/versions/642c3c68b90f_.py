6571cdd9a315b3c6705bb6af3d98af0d31501313
Zach Mason <zmason@delmarit.com>
2016-08-16 09:28:13 -0400

    Re #723. Merged alembic heads

---
+++ alembic/versions/642c3c68b90f_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 642c3c68b90f
+Revises: ('44acec40b170', 'b934f821a42e')
+Create Date: 2016-08-16 13:27:34.874253
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '642c3c68b90f'
+down_revision = ('44acec40b170', 'b934f821a42e')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
