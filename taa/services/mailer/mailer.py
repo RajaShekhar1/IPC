e469f6113ad22283be1dc8b5f271a81b1481bd91
Zach Mason <zmason@delmarit.com>
2016-12-07 14:48:25 -0500

    Re #822. Updated the sending of email to use a config variable for from, allow customization of self enroll address and name to be used for the reply-to header.

--- taa/services/mailer/mailer.py
+++ taa/services/mailer/mailer.py
@@ -6,21 +6,28 @@ from sparkpost.exceptions import SparkPostAPIException
 from taa import app
 
 
-def send_email(to, subject, from_email="noreply@5StarEnroll.com", from_name="5Star Enrollment", html=None, text=None, reply_to=None, track_clicks=True, attachments=None):
+def send_email(to, subject, from_email="noreply@5StarEnroll.com", html=None, text=None, reply_to=None, track_clicks=True, attachments=None, **kwargs):
 
     sparkpost = SparkPost(app.config['SPARKPOST_API_KEY'])
 
     if not attachments:
         # Sparkpost crashes if None is passed in ...
         attachments = []
-
+    
+    if not from_email:
+        from_name = app.config.get('EMAIL_FROM_NAME', '5Star Enrollment')
+        from_email = app.config.get('EMAIL_FROM_ADDRESS', 'noreply@5StarEnroll.com')
+    
+        from_address = '{} <{}>'.format(from_name, from_email)
+    else:
+        from_address = from_email
+    
     try:
         response = sparkpost.transmissions.send(
             recipients=to,
             html=html,
             text=text,
-            from_email=from_email,
-            from_name=from_name,
+            from_email=from_address,
             subject=subject,
             reply_to=reply_to,
             transactional=True,
