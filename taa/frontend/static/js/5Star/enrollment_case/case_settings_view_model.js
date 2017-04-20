acc3e83e5ab89ceb90013f8cd9e34f4963fba090
David Meyer <swankdave@gmail.com>
2017-04-20 14:55:20 -0400

    missed a spot...

--- taa/frontend/static/js/5Star/enrollment_case/case_settings_view_model.js
+++ taa/frontend/static/js/5Star/enrollment_case/case_settings_view_model.js
@@ -88,11 +88,11 @@ var CaseViewModel = function CaseViewModel(case_data, product_choices, can_edit_
   };
 
   self.should_show_max_coverage = function(product) {
-    return !_.includes(["HI", "ACC", "Static Benefit"], product.base_product_type);
+    return !_.includes(["HI", 'HIL01', "ACC", "Static Benefit"], product.base_product_type);
   };
 
   self.should_show_max_age = function(product) {
-    return !_.includes(["HI", "ACC", "Static Benefit"], product.base_product_type);
+    return !_.includes(["HI", 'HIL01', "ACC", "Static Benefit"], product.base_product_type);
   };
 
   self.get_product_effective_date_settings = function (product) {
@@ -111,11 +111,11 @@ var CaseViewModel = function CaseViewModel(case_data, product_choices, can_edit_
   };
 
   self.should_show_max_coverage = function(product) {
-    return !_.includes(["HI", "ACC", "Static Benefit"], product.base_product_type);
+    return !_.includes(["HI", 'HIL01', "ACC", "Static Benefit"], product.base_product_type);
   };
 
   self.should_show_max_age = function(product) {
-    return !_.includes(["HI", "ACC", "Static Benefit"], product.base_product_type);
+    return !_.includes(["HI", 'HIL01', "ACC", "Static Benefit"], product.base_product_type);
   };
 
 
