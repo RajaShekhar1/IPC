e38c8a2be6fe37631291fb94715558c00c598c55
Patrick Falvey <patrickfalvey40@gmail.com>
2017-06-08 10:09:33 -0600

    Add census_report and export_enrollment_report procedures

---
+++ alembic/versions/75522d63df0d_add_census_report_and_export_enrollment_.py
@@ -0,0 +1,22 @@
+"""Add census report and export_enrollment_report procedures
+
+Revision ID: 75522d63df0d
+Revises: 5bbb4335a759
+Create Date: 2017-06-08 16:05:49.537840
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '75522d63df0d'
+down_revision = '5bbb4335a759'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
