5924a864e21877e95fee8f0318eaae0829da8b00
david-meyer-ipconsultinginc <96y5B7J*^Swo>
2017-07-19 12:51:45 -0400

    re-ordering methods

--- taa/api/api_helpers.py
+++ taa/api/api_helpers.py
@@ -3,6 +3,19 @@ import datetime
 from flask import request
 from werkzeug.exceptions import abort
 
+def parse_dates_as_str_from_request():
+    start_date_string = datetime.date(1900, 1, 1).strftime("%Y-%m-%d")
+    end_date_string = datetime.datetime.max.strftime("%Y-%m-%d")
+    if 'start_date' in request.args and \
+            len(request.args.get('start_date')) > 0 and \
+            request.args.get('start_date') != "undefined":
+        start_date_string = request.args.get('start_date')
+    if 'end_date' in request.args and \
+            len(request.args.get('end_date')) > 0 and \
+            request.args.get('end_date') != "undefined":
+        end_date_string = request.args.get('end_date')
+
+    return end_date_string, start_date_string
 
 def parse_dates_from_request():
     end_date_string, start_date_string = parse_dates_as_str_from_request()
@@ -16,13 +29,3 @@ def parse_dates_from_request():
 
     return end_date, start_date
 
-
-def parse_dates_as_str_from_request():
-    start_date_string = datetime.datetime.min.strftime("%Y-%m-%d")
-    end_date_string = datetime.datetime.max.strftime("%Y-%m-%d")
-    if 'start_date' in request.args and len(request.args.get('start_date')) > 0:
-        start_date_string = request.args.get('start_date')
-    if 'start_date' in request.args and len(request.args.get('start_date')) > 0:
-        end_date_string = request.args.get('start_date')
-
-    return end_date_string, start_date_string
