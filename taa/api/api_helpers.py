e73e1e5291775e972fb4e1a0567b6a37608393a7
david-meyer-ipconsultinginc <96y5B7J*^Swo>
2017-07-19 14:30:17 -0400

    added ability to test error email system from api call

--- taa/api/api_helpers.py
+++ taa/api/api_helpers.py
@@ -1,21 +1,25 @@
 import datetime
 
-from flask import request
-from werkzeug.exceptions import abort
+from taa import errors, app
+from taa.api import *
+from flask import Blueprint, request, abort, make_response, Response
+from flask_stormpath import current_user, groups_required, login_required
+
 
 def parse_dates_as_str_from_request():
     start_date_string = datetime.date(1900, 1, 1).strftime("%Y-%m-%d")
     end_date_string = datetime.datetime.max.strftime("%Y-%m-%d")
     if 'start_date' in request.args and \
-            len(request.args.get('start_date')) > 0 and \
-            request.args.get('start_date') != "undefined":
+                    len(request.args.get('start_date')) > 0 and \
+                    request.args.get('start_date') != "undefined":
         start_date_string = request.args.get('start_date')
     if 'end_date' in request.args and \
-            len(request.args.get('end_date')) > 0 and \
-            request.args.get('end_date') != "undefined":
+                    len(request.args.get('end_date')) > 0 and \
+                    request.args.get('end_date') != "undefined":
         end_date_string = request.args.get('end_date')
 
     return end_date_string, start_date_string
+
 
 def parse_dates_from_request():
     end_date_string, start_date_string = parse_dates_as_str_from_request()
@@ -29,3 +33,13 @@ def parse_dates_from_request():
 
     return end_date, start_date
 
+
+bp = Blueprint('system', __name__, url_prefix='/system')
+api_groups = ['admins']
+
+
+@route(bp, '/error_email_test')
+@login_required
+@groups_required(api_groups, all=False)
+def error_email_test():
+    errors.email_exception(app, Exception("test"))
